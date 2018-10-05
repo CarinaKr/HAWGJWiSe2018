137ae8859e30532c9bb0ac58b8b5ce7cc546c9e4
CarinaKr <carina.kraff@t-online.de>
2018-10-05 22:45:31 +0200

    cameraMovement

---
+++ HAWGJWiSe2018/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour {
+
+    public float speed;
+    public Transform mainCamera;
+
+    private Vector2 movement;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Move(Input.GetAxis("CameraHorizontal"), -1*Input.GetAxis("CameraVertical"));
+	}
+
+    public void Move(float inputX,float inputY)
+    {
+        movement = new Vector2(inputX, inputY) * speed*Time.deltaTime;
+        if(movement.magnitude>0.05)
+            mainCamera.Translate(movement);
+    }
+}
---
+++ HAWGJWiSe2018/Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour {
+
+    public Camera mainCamera;
+    public Color mainColor;
+
+    private bool _isAlive;
+    private Plane[] planes;
+    private Collider2D objCollider;
+
+	// Use this for initialization
+	void Start () {
+        planes = GeometryUtility.CalculateFrustumPlanes(mainCamera);
+        objCollider = GetComponent<Collider2D>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Vector2 relPosition = mainCamera.WorldToViewportPoint(transform.position);
+        if (!(relPosition.x>=0 && relPosition.x<=1 && relPosition.y>=0 && relPosition.y<=1))
+        {
+            OutOfFrame();
+        }
+        else
+        {
+            InFrame();
+        }
+    }
+
+    //private void OnBecameInvisible()
+    //{
+    //    OutOfFrame();
+    //}
+
+    public void OutOfFrame()
+    {
+        _isAlive = false;
+        GetComponent<SpriteRenderer>().color = Color.grey;
+        //TODO: Trigger Feuer-Wasser-Sturm
+    }
+    public void InFrame()
+    {
+        _isAlive = true;
+        GetComponent<SpriteRenderer>().color = mainColor;
+    }
+}
