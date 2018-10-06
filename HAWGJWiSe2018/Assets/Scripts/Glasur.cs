236509ed13dab0dc9e820206220a2998f8adb975
BettinaG <bettina.ganter@gmx.de>
2018-10-06 23:19:13 +0200

    Glasuren added

---
+++ HAWGJWiSe2018/Assets/Scripts/Glasur.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Glasur : MonoBehaviour {
+
+    public Enums.PlatformType platformtypes;
+    [Tooltip("order: Feuer, Wasser, Sturm")]
+    public Sprite[] glasuren;
+
+    private SpriteRenderer renderer;
+
+	// Use this for initialization
+	void Start () {
+
+        renderer = GetComponent<SpriteRenderer>();
+        platformtypes = GetComponentInParent<Platform>().platformtype;
+
+        if(platformtypes == Enums.PlatformType.DEFAULT)
+        {
+            gameObject.SetActive(false);
+            return;
+        }
+
+        renderer.sprite = glasuren[(int)platformtypes-1];
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
